3a40d8ad5e0b7a7a623bc14befe839e1a60ec9de
Ivan Posti <ivmposti@gmail.com>
2020-03-24 02:57:13 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import org.jetbrains.intellij.tasks.*
+
 plugins {
     id("org.jetbrains.intellij") version "0.4.16"
-    java
     kotlin("jvm") version "1.3.70"
+    java
 }
 
 group = "com.firsttimeinforever.intellij.pdf.viewer"
@@ -17,11 +19,9 @@ dependencies {
     testCompile("junit", "junit", "4.12")
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    // https://github.com/JetBrains/intellij-community/releases/tag/idea%2F201.6073.9
     // Build against next EAP version
-    version = "201.6073.9-EAP-SNAPSHOT"
+    version = "LATEST-EAP-SNAPSHOT"
 }
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -34,8 +34,13 @@ tasks {
         kotlinOptions.jvmTarget = "1.8"
     }
 }
-tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""
       Add change notes here.<br>
       <em>most HTML tags may be used</em>""")
+}
+
+tasks.withType<RunIdeTask>() {
+    systemProperties["ide.browser.jcef.enabled"] = true
+    jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
 }
