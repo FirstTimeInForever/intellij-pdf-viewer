9130a7aa0d7d90dd5533c3f302d01504a55179b5
Ivan Posti <ivmposti@gmail.com>
2020-03-24 02:02:39 +0300

    Implement basic pdf view with PDF.js
    web-view project should be built separately with
    npm run build

--- src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/ui/editor/ScriptsProvider.kt
+++ src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/ui/editor/ScriptsProvider.kt
@@ -1,11 +1,18 @@
 package com.firsttimeinforever.intellij.pdf.viewer.ui.editor
 
+import com.intellij.openapi.diagnostic.DefaultLogger
+import com.intellij.openapi.diagnostic.Logger
+
+
 class ScriptsProvider {
     companion object {
-        val INDEX = load("/scripts/index.html")
+        private val logger = Logger.getInstance(ScriptsProvider::class.java)
 
-        private fun load(path: String): String {
+        fun load(path: String): String {
             val stream = this::class.java.getResourceAsStream(path)
+            if (stream == null) {
+                logger.error("Could not load file: $path")
+            }
             return stream.use {
                 it.bufferedReader().use {
                     it.readText()
