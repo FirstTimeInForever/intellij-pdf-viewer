8c995a1a61b148b0ae046378dbe6f7d3fe1dfb7a
Ivan Posti <ivmposti@gmail.com>
2020-03-25 02:00:24 +0300

    Add web-view build step to gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 import org.jetbrains.intellij.tasks.*
+import com.moowork.gradle.node.task.*
+import com.moowork.gradle.node.npm.*
 
 plugins {
     id("org.jetbrains.intellij") version "0.4.17"
     kotlin("jvm") version "1.3.70"
+    id("com.github.node-gradle.node") version "2.2.3"
     java
+    idea
 }
 
 group = "com.firsttimeinforever.intellij.pdf.viewer"
@@ -33,15 +37,35 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
+    node {
+        download = true
+    }
 }
+
+idea {
+    module {
+        excludeDirs.add(file("node_modules"))
+    }
+}
+
 tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""
       Add change notes here.<br>
       <em>most HTML tags may be used</em>""")
 }
 
+tasks.register<NpmTask>("webViewBuild") {
+    dependsOn("npm_ci")
+    setArgs(listOf("run", "build"))
+}
+
+tasks.getByName("processResources") {
+    dependsOn("webViewBuild")
+}
+
 tasks.withType<RunIdeTask>() {
     systemProperties["ide.browser.jcef.enabled"] = true
     jvmArgs("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+//    executable(file("$buildDir/jbr/Contents/Home/bin/java"))
 //    jbrVersion("jbr_jcef-11_0_6b765.15")
 }
