4db5b383c906869e9522a030f396fcf4c27ec427
Ivan Posti <ivmposti@gmail.com>
2020-04-17 03:05:03 +0300

    Add plugin icon and update file icons

--- src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/icons/PdfViewerIcons.kt
+++ src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/icons/PdfViewerIcons.kt
@@ -2,9 +2,9 @@ package com.firsttimeinforever.intellij.pdf.viewer.icons
 
 import com.intellij.openapi.util.IconLoader
 
-// TODO: Use SVG icons
+
 class PdfViewerIcons private constructor(){
     companion object {
-        val FILE_ICON = IconLoader.getIcon("/icons/pdfFile.png")
+        val FILE_ICON = IconLoader.getIcon("/icons/pdfFile.svg")
     }
 }
