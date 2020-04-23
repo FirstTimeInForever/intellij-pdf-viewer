2bc2250139a7c69690cc2d4450cf269838f8a9d6
Ivan Posti <ivmposti@gmail.com>
2020-04-23 04:12:54 +0300

    Add message receiver server

---
+++ src/main/web-view/src/app/message-receiver.service.ts
@@ -0,0 +1,42 @@
+import {Injectable, NgZone} from '@angular/core';
+
+
+interface SubscriptionsList {
+    [key: string]: Function[];
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class MessageReceiverService {
+    private subscriptions: SubscriptionsList = {};
+
+    constructor(private zone: NgZone) {
+        window['triggerMessageEvent'] = (eventName, args) => {
+            if (!this.subscriptions[eventName]) {
+                return;
+            }
+            zone.run(() => {
+                this.fireEvent(eventName, args);
+            });
+        };
+    }
+
+    subscribe(eventName: string, callback: Function) {
+        if (!this.subscriptions[eventName]) {
+            this.subscriptions[eventName] = [callback];
+        }
+        else {
+            this.subscriptions[eventName].push(callback);
+        }
+    }
+
+    private fireEvent(eventName: string, args: any) {
+        if (!this.subscriptions[eventName]) {
+            return;
+        }
+        this.subscriptions[eventName].forEach((fn) => {
+            fn.apply(null, [args]);
+        });
+    }
+}
