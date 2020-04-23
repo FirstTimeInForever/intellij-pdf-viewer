02d328eef8d6ad87a78a999d2a3c65db2fb0c094
Ivan Posti <ivmposti@gmail.com>
2020-04-23 07:47:14 +0300

    Use message passing

--- src/main/web-view/src/app/app.component.ts
+++ src/main/web-view/src/app/app.component.ts
@@ -2,19 +2,33 @@ import {Component, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 import {PdfJsViewerComponent} from "ng2-pdfjs-viewer";
+import {MessageReceiverService} from "./message-receiver.service";
+import {MessageSenderService} from "./message-sender";
 
 // const viewerFolder = '64fa8636-e686-4c63-9956-132d9471ce77/assets/pdfjs'
 
 @Component({
     selector: 'app-root',
-    template: `<ng2-pdfjs-viewer #viewer viewerFolder='64fa8636-e686-4c63-9956-132d9471ce77/assets/pdfjs' [download]=false [fullScreen]=false [openFile]=false [viewBookmark]=false pagemode='none'></ng2-pdfjs-viewer>`,
+    template: `<ng2-pdfjs-viewer #viewer viewerId="__uniqueViewerId" (onPageChange)="pageChanged($event)" 
+                                 [page]="actualPage" [download]=false [fullScreen]=false [openFile]=false 
+                                 viewerFolder='64fa8636-e686-4c63-9956-132d9471ce77/assets/pdfjs' 
+                                 [viewBookmark]=false pagemode='none'>
+    </ng2-pdfjs-viewer>`,
     styles: []
 })
 export class AppComponent {
     @ViewChild('viewer')
     private viewer: PdfJsViewerComponent;
 
-    constructor(private http: HttpClient, private route: ActivatedRoute) {
+    actualPage: number;
+
+    pageChanged(pageNumber: number) {
+        this.messageSenderService.triggerEvent("pageChanged", {pageNumber});
+    }
+
+    constructor(private http: HttpClient, private route: ActivatedRoute,
+        private messageReceiverService: MessageReceiverService,
+        private messageSenderService: MessageSenderService) {
         const subscription = this.route.queryParams.subscribe(params => {
             const targetUrl = params['path'];
             if (!targetUrl) {
@@ -29,6 +43,9 @@ export class AppComponent {
                 this.viewer.pdfSrc = res;
                 this.viewer.refresh();
             });
+        });
+        this.messageReceiverService.subscribe("pageSet", (data: any) => {
+            this.actualPage = data.pageNumber;
         });
     }
 }
