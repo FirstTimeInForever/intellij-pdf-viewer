61f3332ff8b629806a14cb13cb196e099d3e9b4e
Ivan Posti <ivmposti@gmail.com>
2020-05-21 18:29:55 +0300

    Refactor stuff

--- src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/icons/PdfViewerIcons.kt
+++ src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/icons/PdfViewerIcons.kt
@@ -2,9 +2,6 @@ package com.firsttimeinforever.intellij.pdf.viewer.icons
 
 import com.intellij.openapi.util.IconLoader
 
-
-class PdfViewerIcons private constructor(){
-    companion object {
-        val FILE_ICON = IconLoader.getIcon("/icons/pdfFile.svg")
-    }
+object PdfViewerIcons {
+    val FILE_ICON = IconLoader.getIcon("/icons/pdfFile.svg")
 }
