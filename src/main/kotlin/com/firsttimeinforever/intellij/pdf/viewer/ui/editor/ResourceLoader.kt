be41ccf5b89ce13ead512465bc9aba314ba16ee1
Ivan Posti <ivmposti@gmail.com>
2020-05-21 17:56:42 +0300

    Fix jar path issues on windows

--- src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/ui/editor/ResourceLoader.kt
+++ src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/ui/editor/ResourceLoader.kt
@@ -3,13 +3,19 @@ package com.firsttimeinforever.intellij.pdf.viewer.ui.editor
 import java.io.File
 import java.io.FileNotFoundException
 
+object ResourceLoader {
+    fun load(file: File): ByteArray {
+        val targetPath = ensureCorrectFormedPath(file)
+        val stream = this::class.java.getResourceAsStream(targetPath)
+            ?: throw FileNotFoundException("Could not load resource file: $targetPath")
+        return stream.readAllBytes()
+    }
 
-class ResourceLoader {
-    companion object {
-        fun load(file: File): ByteArray {
-            val stream = this::class.java.getResourceAsStream(file.toString())
-                ?: throw FileNotFoundException("Could not load resource file: $file")
-            return stream.readAllBytes()
+    // Fix incorrect pathing on windows, duh
+    private fun ensureCorrectFormedPath(file: File): String {
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            return file.toString().replace('\\', '/')
         }
+        return file.toString()
     }
 }
