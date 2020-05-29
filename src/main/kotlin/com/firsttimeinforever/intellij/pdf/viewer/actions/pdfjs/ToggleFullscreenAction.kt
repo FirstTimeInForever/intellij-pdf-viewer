d96af96b6631da217a3a329f4e3bfcd61d583fb8
Ivan Posti <ivmposti@gmail.com>
2020-05-29 22:05:44 +0300

    Add fullscreen mode action

---
+++ src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/pdfjs/ToggleFullscreenAction.kt
@@ -0,0 +1,16 @@
+package com.firsttimeinforever.intellij.pdf.viewer.actions.pdfjs
+
+import com.intellij.ide.actions.TogglePresentationModeAction
+import com.intellij.ide.ui.UISettings
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ToggleFullscreenAction: PdfEditorPdfjsAction() {
+    override val disableInIdePresentationMode = false
+
+    override fun actionPerformed(event: AnActionEvent) {
+        TogglePresentationModeAction.setPresentationMode(
+            event.project,
+            !UISettings.instance.presentationMode
+        )
+    }
+}
