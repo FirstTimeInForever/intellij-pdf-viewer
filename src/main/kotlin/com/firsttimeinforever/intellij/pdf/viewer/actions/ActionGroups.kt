af662e884b337effc665010ff035e59a502599e0
Ivan Posti <ivmposti@gmail.com>
2020-07-02 20:10:47 +0300

    Disable sidebar action group in presentation mode

--- src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/ActionGroups.kt
+++ src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/ActionGroups.kt
@@ -1,6 +1,10 @@
 package com.firsttimeinforever.intellij.pdf.viewer.actions
 
+import com.firsttimeinforever.intellij.pdf.viewer.ui.editor.PdfFileEditor
+import com.firsttimeinforever.intellij.pdf.viewer.ui.editor.panel.jcef.PdfFileEditorJcefPanel
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.fileEditor.FileEditorManager
 
 class PdfFileEditorActionGroup: DefaultActionGroup() {}
 
@@ -10,4 +14,18 @@ class PdfEditorToolbarSearchActionGroup: DefaultActionGroup() {}
 
 class PdfEditorRightToolbarActionGroup: DefaultActionGroup() {}
 
-class PdfEditorSidebarViewModeActionGroup: DefaultActionGroup() {}
+class PdfEditorSidebarViewModeActionGroup: DefaultActionGroup() {
+    override fun isPopup(): Boolean = true
+
+    override fun update(event: AnActionEvent) {
+        event.presentation.isEnabled = false
+        event.project?.let { project ->
+            FileEditorManager.getInstance(project).selectedEditor?.also {
+                if (it is PdfFileEditor && it.viewPanel is PdfFileEditorJcefPanel) {
+                    event.presentation.isEnabled =
+                        !it.viewPanel.presentationModeController.isPresentationModeActive()
+                }
+            }
+        }
+    }
+}
