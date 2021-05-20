8c53d1414d5df218605960fb9af56fbaa6b5dab8
Ivan Posti <ivan.posti@jetbrains.com>
2021-05-20 21:31:29 +0300

    Add hard reload action

--- plugin/src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/view/PdfHardReloadViewAction.kt
+++ plugin/src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/view/PdfHardReloadViewAction.kt
@@ -1,15 +1,15 @@
 package com.firsttimeinforever.intellij.pdf.viewer.actions.view
 
 import com.firsttimeinforever.intellij.pdf.viewer.actions.PdfDumbAwareAction
+import com.firsttimeinforever.intellij.pdf.viewer.actions.ViewModeAwareness
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-class PdfReloadViewAction : PdfDumbAwareAction() {
+class PdfHardReloadViewAction : PdfDumbAwareAction(ViewModeAwareness.BOTH) {
   override fun actionPerformed(event: AnActionEvent) {
-    findController(event)?.reload(tryToPreserveState = true)
+    findController(event)?.reload(tryToPreserveState = false)
   }
 
   override fun update(event: AnActionEvent) {
     event.presentation.isEnabled = true
   }
 }
-
