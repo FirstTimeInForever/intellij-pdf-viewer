3f4eee238b85dcb540faa37e9f373b630536aa2e
Abby Berkers <abbyberkers@gmail.com>
2023-04-22 12:12:32 +0200

    Enable color inversion

--- web-view/viewer/src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/application/pdfjs/ThemeUtils.kt
+++ web-view/viewer/src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/application/pdfjs/ThemeUtils.kt
@@ -5,6 +5,11 @@ import kotlinx.browser.window
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.HTMLElement
+import org.w3c.dom.asList
+import org.w3c.dom.css.CSSImportRule
+import org.w3c.dom.css.CSSStyleRule
+import org.w3c.dom.css.CSSStyleSheet
+import org.w3c.dom.css.get
 
 object ThemeUtils {
   // https://github.com/allefeld/atom-pdfjs-viewer/issues/4#issuecomment-622942606
@@ -48,5 +53,26 @@ object ThemeUtils {
       setProperty(Internals.StyleVariables.treeItemColor, viewTheme.foreground)
       setProperty(Internals.StyleVariables.treeItemHoverColor, viewTheme.foreground)
     }
+
+    val css = window.document.styleSheets[0] as CSSStyleSheet
+    val pdfViewerCss =
+      css.cssRules.asList().filterIsInstance<CSSImportRule>().find { it.href == "pdf_viewer.css" }?.styleSheet
+
+    val pageViewerRule = pdfViewerCss?.cssRules?.asList()?.filterIsInstance<CSSStyleRule>()
+      ?.find { it.selectorText == ".pdfViewer .page" } as? CSSStyleRule
+    pageViewerRule?.style?.removeProperty("-o-border-image")
+    pageViewerRule?.style?.removeProperty("border-image")
+
+    val thumbnailRule = css.cssRules.asList().filterIsInstance<CSSStyleRule>().find { it.selectorText == ".thumbnailImage" }
+    thumbnailRule?.style?.removeProperty("box-shadow")
+
+    //language=CSS
+    val rule =
+      """
+      .page, .thumbnailImage {
+        filter: invert(${viewTheme.colorInvertIntensity}%);
+      }
+      """.trimIndent()
+    pdfViewerCss?.insertRule(rule, pdfViewerCss.cssRules.length)
   }
 }
