bd6ee3045161d38a619de04a1ce58ea8b847a73c
Abby Berkers <abbyberkers@gmail.com>
2023-04-22 14:58:28 +0200

    WIP add icon for inverting colors

--- plugin/src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/view/PdfToggleInvertDocumentColorsAction.kt
+++ plugin/src/main/kotlin/com/firsttimeinforever/intellij/pdf/viewer/actions/view/PdfToggleInvertDocumentColorsAction.kt
@@ -5,12 +5,26 @@ import com.firsttimeinforever.intellij.pdf.viewer.actions.ViewModeAwareness
 import com.firsttimeinforever.intellij.pdf.viewer.settings.PdfViewerSettings
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.util.IconLoader
 
 class PdfToggleInvertDocumentColorsAction : PdfToggleAction(ViewModeAwareness.BOTH), DumbAware {
+
   override fun isSelected(event: AnActionEvent) = PdfViewerSettings.instance.invertDocumentColors
 
   override fun setSelected(event: AnActionEvent, state: Boolean) {
     PdfViewerSettings.instance.invertDocumentColors = state
     PdfViewerSettings.instance.notifyListeners()
+  }
+
+  override fun update(event: AnActionEvent) {
+    event.presentation.apply {
+      icon = if (PdfViewerSettings.instance.invertDocumentColors) {
+        IconLoader.getIcon("expui/meetNewUi/lightTheme.svg", this::class.java.classLoader)
+      } else {
+        IconLoader.getIcon("expui/meetNewUi/darkTheme.svg", this::class.java.classLoader)
+      }
+      selectedIcon = icon
+    }
+    super.update(event)
   }
 }
